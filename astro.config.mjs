ec84536c91e250764e1891aecffe12d80901b175
robrrto <robertoclc@gmail.com>
2026-08-11 19:33:56 -0300

    fix: desativa cssCodeSplit para otimizar carregamento crítico

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,4 +5,9 @@ import sitemap from "@astrojs/sitemap";
 export default defineConfig({
   site: "https://quiztimeplay.com.br",
   integrations: [sitemap()],
+  vite: {
+    build: {
+      cssCodeSplit: false,
+    },
+  },
 });
